b8f64d58cbef81436f23f86e351c0847829279a4
Lakshmi241995 <lakshmi241995@gmail.com>
2025-04-07 14:54:56 +0530

    Added Operators_3 file with new print statement

--- src/test/java/Operators_2.java
+++ src/test/java/Operators_2.java
@@ -1,5 +1,6 @@
 public class Operators_2 {
 
-    public static void main(String[] args){System.out.println("Hello world");
+    public static void main(String[] args){
+        System.out.println("Hello world");
     }
 }
--- src/test/java/Operators_3.java
+++ src/test/java/Operators_3.java
@@ -3,5 +3,6 @@ public class Operators_3
     public static void main(String arg[]){
 
         System.out.println("Hello Automation");
+        System.out.println("Learning Automation");
     }
 }
