3a214775fb9c4721a680a183af64b5bd5b4e2776
Lakshmi241995 <lakshmi241995@gmail.com>
2025-04-07 14:49:34 +0530

    Added Operators_3 file

--- src/test/java/Operators_2.java
+++ src/test/java/Operators_2.java
@@ -1,6 +1,5 @@
 public class Operators_2 {
 
-    public static void main(String[] args){
-        System.out.println("Hello world");
+    public static void main(String[] args){System.out.println("Hello world");
     }
 }
---
+++ src/test/java/Operators_3.java
@@ -0,0 +1,7 @@
+public class Operators_3
+{
+    public static void main(String arg[]){
+
+        System.out.println("Hello Automation");
+    }
+}
